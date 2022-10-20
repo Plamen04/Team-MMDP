af32747c2f7744f45a9375b30b5964d5cf69eec2
TEACHERS-STEM-2 <TEACHERS-STEM-2@DESKTOP-ITI7R6F>
2022-10-20 09:20:17 +0300

    Created Data Models User and Shifts

---
+++ EMDataAccess/Models/Shifts.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EMDataAccess.Models
+{
+    public class Shifts
+    {
+        public int ShiftCount { get; set; }
+        public string ShiftName { get; set; }
+        public int ShiftId { get; set; }
+    }
+}
