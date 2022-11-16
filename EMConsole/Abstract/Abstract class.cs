ce9d6408e96a0abf35dc7fe95cdddf1bc17f7a67
Svetlana Georgieva <sv.georgieva@pgzaimov.com>
2022-11-16 13:35:04 +0200

    added abstract class

---
+++ EMConsole/Abstract/Abstract class.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EMConsole.Abstract
+{
+    public abstract class Abstract_class
+    {
+
+    }
+}
